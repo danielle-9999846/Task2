df2b0f9794eddef88a66c93d4ac450d4e5ac77fa
danielle-9999846 <53239113+danielle-9999846@users.noreply.github.com>
2019-07-31 11:33:29 +1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter your 4 digit pin number: ");
+            int pin = int.Parse(Console.ReadLine());
+
+            if (pin > 9999 || pin < 1000)
+                Console.Write("please enter your 4 digit pin number again: ");
+            int pin2 = int.Parse(Console.ReadLine());
+            if (pin == pin2)
+                Console.WriteLine("Pin accepted");
+
+            else
+            {
+                Console.WriteLine("there's an issue with your pin...");
+            }
+            
+            
+            else
+
+             {
+                Console.WriteLine("Pin accepted");
+              }
+            
+        } 
+
+            
+        
+    }
+}
